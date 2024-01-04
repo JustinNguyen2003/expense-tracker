8932fa705912c272a2ea2fa7fc16633c3e8e3125
Justin Nguyen <justinator999@gmail.com>
2024-01-04 15:28:44 -0800

    started setting up backend

---
+++ controllers/transactionController.js
@@ -0,0 +1,20 @@
+// @desc    Get all transactions
+// @route   GET /api/v1/transactions
+// @access  Public
+exports.getTransactions = (req,res,next) => {
+    res.send('GET Transactions');
+}
+
+// @desc    Add transactions
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransactions = (req,res,next) => {
+    res.send('POST Transactions');
+}
+
+// @desc    Delete a transactions
+// @route   DELETE /api/v1/transactions/:id
+// @access  Public
+exports.deleteTransactions = (req,res,next) => {
+    res.send('DELETE Transactions');
+}
